81bf03f99d9422f1b87f00cef4da49cb23ef9953
rifandiif <65202945+rifandiif@users.noreply.github.com>
2025-05-11 17:50:15 +0700

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image"
 import { motion } from "framer-motion"
 import { AnimatedText } from "./animated-text"
-import { BackgroundBeams } from "@/components/ui/background-beams";
+
 
 
 const WavingHand = () => {
